b3a4145f8c5fcb12493a3922198ccc5d6c495df4
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2024-06-16 09:39:56 -0400

    refactor: clean up

--- apps/kwstriping/app/products/[slug]/page.tsx
+++ apps/kwstriping/app/products/[slug]/page.tsx
@@ -12,9 +12,6 @@ import Layout from '@kwstriping/app/client/Layout';
 const API_URL = process.env.NEXT_PUBLIC_API_URL || process.env.API_URL;
 if (!API_URL) throw new Error('API_URL is not set.');
 
-// TODO
-// https://nextjs.org/docs/app/building-your-application/optimizing/metadata#dynamic-metadata
-
 const makeClient = () => {
   return createClient({
     url: API_URL,
@@ -24,58 +21,31 @@ const makeClient = () => {
 
 export const { getClient } = registerUrql(makeClient);
 
-export const metadata: Metadata = {
-  title: 'Page',
-};
-
-// const { name: shopName } = useShopSettings();
-// const productName = product.seoTitle || product.name;
-// const title = productName ? `${productName} - ${shopName}` : shopName;
-
-// const description = product?.seoDescription || '';
-// const thumbnailUrl = product.thumbnail?.url || '';
-// const thumbnailAlt = product.thumbnail?.alt || title;
-
-// return (
-//   <NextSeo
-//     title={title}
-//     description={description}
-//     openGraph={{
-//       title,
-//       description,
-//       images: [
-//         {
-//           url: thumbnailUrl,
-//           alt: thumbnailAlt,
-//         },
-//       ],
-//       site_name: 'Tempo Tutorial',
-//     }}
-//   />
-// );
-
-// NOT FOUND SEO
-// const { name: shopName } = useShopSettings();
-// const title = `Page Not found - ${shopName}`;
-// const description = 'Page not found.';
-
-// return (
-//   <NextSeo
-//     title={title}
-//     description={description}
-//     openGraph={{
-//       title,
-//       description,
-//       images: [
-//         {
-//           url: 'https://og-image.vercel.app/React%20Storefront.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-black.svg&images=https%3A%2F%2Ftempo.io%2Fstatic%2Flogo-ad1b99aa7c6f5acf58a61640af760cfd.svg',
-//           alt: 'Tempo tutorial hero image',
-//         },
-//       ],
-//       site_name: 'Tempo Tutorial',
-//     }}
-//   />
-// );
+export async function generateMetadata({ slug }: { slug: string }): Promise<Metadata> {
+  const result = await getClient().query(productBySlugQueryDocument, { slug }).toPromise();
+  const product = result?.data?.product;
+  if (!product) return {};
+  const productName = product.seoTitle || product.name;
+  const title = productName;
+  const description = product?.seoDescription || '';
+  const thumbnailUrl = product.thumbnail?.url || '';
+  const thumbnailAlt = product.thumbnail?.alt || title;
+  const siteName = ''; // TODO
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: thumbnailUrl,
+          alt: thumbnailAlt,
+        },
+      ],
+    },
+  };
+}
 
 const productBySlugQueryDocument = gql(`
   query ProductBySlug($slug: String!, $channel: String) {
