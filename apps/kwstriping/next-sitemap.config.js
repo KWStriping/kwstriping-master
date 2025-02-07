e48d9b89a88553d9119bfe7763a795871217938b
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2025-02-06 21:34:29 -0500

    fix: set siteUrl

--- apps/kwstriping/next-sitemap.config.js
+++ apps/kwstriping/next-sitemap.config.js
@@ -1,7 +1,12 @@
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
+
+dotenvExpand.expand(dotenv.config());
+
 const excludedPaths = ['/cart', '/checkout', '/account/*'];
 
 export default {
-  siteUrl: process.env.BASE_URL,
+  siteUrl: process.env.BASE_URL || 'https://kwstriping.com',
   generateRobotsTxt: true,
   exclude: excludedPaths + ['/sitemap.xml'],
   robotsTxtOptions: {
